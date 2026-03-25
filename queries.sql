3df61a0cde385bc13feb8da5f9067895de218625
utkarshps3110-gif <utkarshps3110@gmail.com>
2026-03-25 18:26:20 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,25 @@
+-- 1. Customer List
+SELECT firstname, lastname, country
+FROM customer;
 
+-- 2. Customers from USA
+SELECT firstname, lastname
+FROM customer
+WHERE country = 'USA';
+
+-- 3. Total Sales by Country
+SELECT billingcountry,
+SUM(total) AS total_sales
+FROM invoice
+GROUP BY billingcountry
+ORDER BY total_sales DESC;
+
+-- 4. Order Categories
+SELECT invoiceid,
+total,
+CASE
+WHEN total >= 15 THEN 'High Value'
+WHEN total >= 5 THEN 'Medium Value'
+ELSE 'Low Value'
+END AS order_category
+FROM invoice;
